6a226715638e6d54c4ced891238a1351a4dad4b7
川路慎一 <bowmoq@gmail.com>
2019-08-03 11:02:53 +0900

    add bestTimeToPartySmart

--- puzzled-python/chapter02.py
+++ puzzled-python/chapter02.py
@@ -2,6 +2,10 @@ sched = [(6, 8), (6, 12), (6, 7), (7, 8),
          (7, 10), (8, 9), (8, 10), (9, 12),
          (9, 10), (10, 11), (10, 12), (11, 12)]
 
+sched2 = [(6.0, 8.0), (6.5, 12.0), (6.5, 7.0), (7.0, 8.0),
+          (7.5, 10.0), (8.0, 9.0), (8.0, 10.0), (9.0, 12.0),
+          (9.5, 10.0), (10.0, 11.0), (10.0, 12.0), (11.0, 12.0)]
+
 def bestTimeToParty(schedule):
   start = schedule[0][0]
   end = schedule[0][1]
@@ -11,7 +15,7 @@ def bestTimeToParty(schedule):
     end = max(c[1], end)
 
   count = celebrityDensity(schedule, start, end)
-  
+
   maxcount = 0
   for i in range(start, end + 1):
     if count[i] > maxcount:
@@ -30,4 +34,37 @@ def celebrityDensity(schedule, start, end):
         count[i] += 1
   return count
 
+def bestTimeToPartySmart(schedule):
+  times = []
+  for c in schedule:
+    times.append((c[0], 'start'))
+    times.append((c[1], 'end'))
+  sortList(times)
+  maxcount, time = chooseTime(times)
+  print('Best time to attend the party is at', time, 'o\'clock',
+        ':', maxcount, 'celebrities will be attending!')
+
+def sortList(tlist):
+  for ind in range(len(tlist) - 1):
+    iSm = ind
+    for i in range(ind, len(tlist)):
+      if tlist[iSm][0] > tlist[i][0]:
+        iSm = i
+    tlist[ind], tlist[iSm] = tlist[iSm], tlist[ind]
+
+def chooseTime(times):
+  rcount = 0
+  maxcount = time = 0
+  for t in times:
+    if t[1] == 'start':
+      rcount += 1
+    elif t[1] == 'end':
+      rcount -= 1
+    if rcount > maxcount:
+      maxcount = rcount
+      time = t[0]
+  return maxcount, time
+
+
 bestTimeToParty(sched)
+bestTimeToPartySmart(sched2)
