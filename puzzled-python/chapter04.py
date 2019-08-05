ed793699dcbad3e9bbc164c935acdccaa5717686
川路慎一 <bowmoq@gmail.com>
2019-08-06 07:41:39 +0900

    add EightQueens

--- puzzled-python/chapter04.py
+++ puzzled-python/chapter04.py
@@ -41,4 +41,49 @@ def FourQueens(n=4):
     board[i][0] = 0
   return
 
+# 1次元配列
+def noConflicts1(board, current):
+  for i in range(current):
+    if(board[i] == board[current]):
+      return False
+    if(current - i == abs(board[current] - board[i])):
+      return False
+  return True
+
+def EightQueens(n=8):
+  board = [-1] * n
+  for i in range(n):
+    board[0] = i
+    for j in range(n):
+      board[1] = j
+      if not noConflicts1(board, 1):
+        continue
+      for k in range(n):
+        board[2] = k
+        if not noConflicts1(board, 2):
+          continue
+        for l in range(n):
+          board[3] = l
+          if not noConflicts1(board, 3):
+            continue
+          for m in range(n):
+            board[4] = m
+            if not noConflicts1(board, 4):
+              continue
+            for o in range(n):
+              board[5] = o
+              if not noConflicts1(board, 5):
+                continue
+              for p in range(n):
+                board[6] = p
+                if not noConflicts1(board, 6):
+                  continue
+                for q in range(n):
+                  board[7] = q
+                  if noConflicts1(board, 7):
+                    print(board)
+  return
+
+
 FourQueens()
+EightQueens()
